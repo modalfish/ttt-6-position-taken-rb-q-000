b85195563ef759a7bc066857340a21e50f8641fd
modalfish <ben.alley@gmail.com>
2016-03-24 01:51:50 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,19 @@
 # code your #position_taken? method here!
+def position_taken?(board, position)
+  x = "X"
+  o = "O"
+  space = " "
+  blank = ""
+  nilo = nil
+  if board[position] == x
+    true
+  elsif board[position] == o
+    true
+  elsif board[position] == space
+    false
+  elsif board[position] == blank
+    false
+  elsif board[position] == nilo
+    false
+  end
+end
